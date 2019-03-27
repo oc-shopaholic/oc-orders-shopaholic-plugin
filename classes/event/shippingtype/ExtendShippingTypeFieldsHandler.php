e6844345046a657d441b5125f9800f37830df4bc
Andrey Kharanenka <a.khoronenko@lovata.com>
2019-03-27 18:07:31 +0300

    Added CheckRestrictionInterface, ShippingPriceProcessorInterface interfaces. Code refactoring

--- classes/event/shippingtype/ExtendShippingTypeFieldsHandler.php
+++ classes/event/shippingtype/ExtendShippingTypeFieldsHandler.php
@@ -11,31 +11,6 @@ use Lovata\OrdersShopaholic\Controllers\ShippingTypes;
  */
 class ExtendShippingTypeFieldsHandler extends AbstractBackendFieldHandler
 {
-//        $obEvent->listen(OrderProcessor::EVENT_GET_SHIPPING_PRICE, function ($arOrderData) {
-//
-//            $obShipping = ShippingType::find($arOrderData['shipping_type_id']);
-//
-//            $obCart = CartProcessor::instance()->getCartObject();
-//
-//            if (empty($obShipping->method) || !class_exists($obShipping->method)) {
-//
-//                throw new Exception(Lang::get("lovata.ordersshopaholic::lang.shipping_type.handler.empty_or_not_exists"));
-//            }
-//
-//            return (new $obShipping->method)->run($obShipping, $obCart);
-//        });
-//
-//        $obEvent->listen(ShippingType::EVENT_GET_SHIPPING_PRICE, function ($obShipping) {
-//
-//            $obCart = CartProcessor::instance()->getCartObject();
-//
-//            if (empty($obShipping->method) || !class_exists($obShipping->method)) {
-//
-//                throw new Exception(Lang::get("lovata.ordersshopaholic::lang.shipping_type.handler.empty_or_not_exists"));
-//            }
-//
-//            return (new $obShipping->method)->run($obShipping, $obCart);
-//        });
     /**
      * Extend form fields
      * @param \Backend\Widgets\Form $obWidget
