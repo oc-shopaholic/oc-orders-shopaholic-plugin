a8d14ba4a2e422ba8c032f47592af69ea64ad6a7
Sergey Zakharevich <wobqqq@gmail.com>
2020-08-04 09:39:51 +0300

    NC-51 #comment Add annotations is_file_access, file_access_id fields to OrderPosition model

--- models/OrderPosition.php
+++ models/OrderPosition.php
@@ -33,8 +33,6 @@ use Lovata\OrdersShopaholic\Classes\PromoMechanism\OrderPromoMechanismProcessor;
  * @property string                                                    $item_type
  * @property string                                                    $currency_symbol
  * @property string                                                    $currency_code
- * @property bool                                                      $is_file_access
- * @property integer                                                   $file_access_id
  *
  * @property string                                                    $price
  * @property float                                                     $price_value
@@ -90,6 +88,10 @@ use Lovata\OrdersShopaholic\Classes\PromoMechanism\OrderPromoMechanismProcessor;
  * @property int                                                       $subscription_access_id
  * @property \Lovata\SubscriptionsShopaholic\Models\SubscriptionAccess $subscription_access
  * @method static \October\Rain\Database\Relations\BelongsTo|\Lovata\SubscriptionsShopaholic\Models\SubscriptionAccess subscription_access()
+ *
+ * Downloadable file for Shopaholic
+ * @property bool                                                      $is_file_access
+ * @property integer                                                   $file_access_id
  */
 class OrderPosition extends Model
 {
